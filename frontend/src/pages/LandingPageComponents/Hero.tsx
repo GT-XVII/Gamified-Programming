15de914942384999f5fcdee88805ad61d9660165
GT-XVII <liamthomson118@gmail.com>
2025-04-30 18:56:26 +0200

    Fixed, had to force white on text elements

--- frontend/src/pages/LandingPageComponents/Hero.tsx
+++ frontend/src/pages/LandingPageComponents/Hero.tsx
@@ -6,10 +6,10 @@ export const Hero: React.FC = () => {
       <div className="flex gap-5 max-md:flex-col">
         <div className="flex flex-col w-[42%] max-md:ml-0 max-md:w-full">
           <div className="flex flex-col w-full text-white max-md:max-w-full">
-            <h1 className="text-left overflow-hidden flex-1 shrink px-4 pt-3 pb-20 text-7xl tracking-tighter rounded-lg border border-solid bg-white bg-opacity-0 border-zinc-300 border-opacity-0 font-[number:var(--sds-typography-title-page-font-weight)] leading-[80px] min-h-[498px] min-w-[240px] max-md:max-w-full max-md:text-4xl max-md:leading-[53px]">
+            <h1 className="text-left overflow-hidden flex-1 shrink px-4 pt-3 pb-20 text-7xl tracking-tighter rounded-lg border border-solid bg-white/10 border-zinc-300 border-opacity-0 font-[number:var(--sds-typography-title-page-font-weight)] leading-[80px] min-h-[498px] min-w-[240px] max-md:max-w-full max-md:text-4xl max-md:leading-[53px] text-white">
               Big Bold Title Text That Captures Readers Attention
             </h1>
-            <p className=" text-left overflow-hidden flex-1 shrink px-4 pt-3 pb-14 text-xl tracking-tight leading-6 rounded-lg border border-solid bg-white bg-opacity-0 border-zinc-300 border-opacity-0 font-[number:var(--sds-typography-subheading-font-weight)] min-h-[112px] min-w-[240px] max-md:max-w-full">
+            <p className=" text-left overflow-hidden flex-1 shrink px-4 pt-3 pb-14 text-xl tracking-tight leading-6 rounded-lg border border-solid bg-white/10 border-zinc-300 border-opacity-0 font-[number:var(--sds-typography-subheading-font-weight)] min-h-[112px] min-w-[240px] max-md:max-w-full text-white">
               Some descriptive text. Think of something cool to write here.
             </p>
           </div>
