5ba44bad91885cea4ccb32e923b0278246dd6e2a
GT-XVII <liamthomson118@gmail.com>
2025-05-21 09:05:33 +0200

    TypeScript venv variable definition.

--- frontend/src/pages/QuizPage.tsx
+++ frontend/src/pages/QuizPage.tsx
@@ -1,3 +1,4 @@
+/// <reference types="vite/client" />
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import QuizTask from "./quizcomponents/QuizTask";
